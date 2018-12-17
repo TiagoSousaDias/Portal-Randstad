ce7e93271b18f26536d7d0d0d4436dcf449d464e
Tiago Dias <tiago024@gmail.com>
2018-12-17 11:05:24 +0000

    test

---
+++ Portal Randstad/BDA_DS.cs
@@ -0,0 +1,22 @@
+﻿namespace Portal_Randstad
+{
+
+
+    partial class BDA_DS
+    {
+        partial class ConcelhoDataTable
+        {
+        }
+
+        partial class dadospessDataTable
+        {
+        }
+    }
+}
+
+namespace Portal_Randstad.BDA_DSTableAdapters {
+    
+    
+    public partial class dadospessTableAdapter {
+    }
+}
--- Portal Randstad/Properties/Settings.Designer.cs
+++ Portal Randstad/Properties/Settings.Designer.cs
@@ -8,22 +8,30 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Portal_Randstad.Properties
-{
-
-
+namespace Portal_Randstad.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"\\\\edp.pt\\edpsc\\EDPSC_DRC-CC_Operaç" +
+            "ão Odivelas\\Coordenação\\Planeamento e Controlo\\UpgradeCtrl\\UpgradeCtrl\\bin\\Debug" +
+            "\\Portal Randstad\\Portal Randstad\\bin\\Portal Randstad\\bdactiva2.accdb\"")]
+        public string BDActiva {
+            get {
+                return ((string)(this["BDActiva"]));
             }
         }
     }
